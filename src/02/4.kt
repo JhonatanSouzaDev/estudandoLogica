35db8489dfbfe2fc12f7fdce5e59813ec7a94699
Jhonatan <xxjhowxx@gmail.com>
2025-03-19 08:01:58 -0300

    4ª pasta de exercícios. if/else

--- src/02/4.kt
+++ src/02/4.kt
@@ -1,5 +1,37 @@
 package `02`
 
 fun main () {
+println("Informe o primeiro valor: ")
+    val valor1 = readln().toInt()
 
+    println("Informe o segundo valor: " )
+    val valor2 = readln().toInt()
+
+    println("Informe o terceiro valor: ")
+    val valor3 = readln().toInt()
+
+    val maior: Int
+    val segundomaior: Int
+
+    if(valor1 > valor2){
+        if(valor1 > valor3){
+            maior = valor1
+            segundomaior = if (valor2 > valor3) valor2 else valor3
+
+        }else {
+            maior = valor3
+            segundomaior = valor1
+        }
+    }else {
+        if(valor2 > valor3){
+            maior = valor2
+            segundomaior = if (valor1 > valor3) valor1 else valor3
+        }else {
+            maior = valor3
+            segundomaior = valor2
+        }
+    }
+
+    val somaDosDoisMaiores = maior + segundomaior
+    println("A soma dos dois maiores valores é: $somaDosDoisMaiores")
 }
