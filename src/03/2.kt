f666a5a992a973d2644b5d90562e7ec1cf5967be
Jhonatan <xxjhowxx@gmail.com>
2025-04-10 08:41:47 -0300

    pasta 3, exercicio 2 - loop em for, com uso de Thread.

---
+++ src/03/2.kt
@@ -0,0 +1,14 @@
+package `03`
+
+import kotlin.concurrent.thread
+
+//entendo que usar Thread não é recomendado, usa- se Coroutines, que é mais leve e mais seguro.
+
+fun main() {
+
+    for (i in 30 downTo 1) {
+        println(i)
+        Thread.sleep(1000)
+    }
+    print("BOOOOM!")
+}
