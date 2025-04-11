aeeb2d65dd5ecb84de4cdf4e7097327738606d05
Jhonatan <xxjhowxx@gmail.com>
2025-04-11 09:11:46 -0300

    pasta 3, exercicio 4. for em média aritimética.

---
+++ src/03/5.kt
@@ -0,0 +1,24 @@
+package `03`
+
+fun main() {
+
+    println("Informe o primeiro número inteiro (menor): ")
+    val primeiroNumero = readln().toInt()
+
+    println("Informe o segundo número inteiro (MAIOR)")
+    val segundoNumero = readln().toInt()
+
+    if (primeiroNumero > segundoNumero) {
+        println("O primeiro número deve ser menor que o segundo número")
+        return
+    }
+    var soma = 0
+    var quantidade = 0
+
+    for (i in primeiroNumero..segundoNumero) {
+        soma += i
+        quantidade++
+    }
+    val media = soma.toDouble() / quantidade
+    println("A média aritimética de $primeiroNumero e $segundoNumero é de $media")
+}
